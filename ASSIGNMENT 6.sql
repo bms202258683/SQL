649346f9ac6122086f3b47ab8291786c3cdf53e9
bms202258683 <160068692+bms202258683@users.noreply.github.com>
2024-04-06 18:07:18 +0300

    Add files via upload

---
+++ ASSIGNMENT 6.sql
@@ -0,0 +1,33 @@
+-- CREATE DATABASE universityDB;
+-- USE universityDB;
+-- CREATE TABLE students(
+	-- SudentID INT PRIMARY KEY,
+    -- FirstName VARCHAR(50),
+    -- LastName VARCHAR(50),
+	-- Age INT,
+    -- Major VARCHAR(50)
+-- );
+-- INSERT INTO students(SudentID, FirstName, LastName, Age, Major)
+-- VALUE
+-- ('1', 'Daniel', 'Kariuki', '20', 'Medicine'),
+-- ('2', 'Hiram', 'Mburu', '16', 'Law'),
+-- ('3', 'John', 'Franco', '23', 'Engineering'),
+-- ('4', 'Mary', 'Joy', '25', 'BCOM'),
+-- ('5', 'Franchesca', 'Nzalo', '20', 'Chef');
+
+-- SELECT * FROM students
+-- ALTER TABLE students
+-- ADD GPA decimal;
+
+-- SELECT * FROM students
+-- CREATE TABLE Courses(
+	-- CourseID INT PRIMARY KEY,
+    -- CourseName VARCHAR(100),
+    -- Instructor VARCHAR(100),
+    -- Credits INT
+-- );
+-- SELECT*FROM courses
+
+
+
+
